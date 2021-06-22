6f6ee2fea050c71270b925fc453277c1102d31c0
Igor Fellows <igor.fpp96@gmail.com>
2021-06-22 19:16:20 -0300

    Adiciona o 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const studantName = { name };
+  studantName.feedback = () => 'Eita pessoa boa!';
+  return studantName;
+};
 
 module.exports = createStudent;
