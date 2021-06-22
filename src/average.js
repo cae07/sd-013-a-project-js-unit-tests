845e56df48c1e4238fb648011993bece12d0294a
Japa <wesleyy_@live.com>
2021-06-22 16:22:06 -0300

    Adicionado requisito 1 e 2

--- src/average.js
+++ src/average.js
@@ -12,8 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = array => {
+  let soma = 0;
+
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    soma += array[i];
+  }
+    soma = Math.round(soma / array.length);
+    return soma;
 };
+
+console.log (average([1, 2, 3, '4' , 5]));
 
 module.exports = average;
