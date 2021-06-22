273a19a4592722a4530f49bed5240aa065b21037
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-22 18:38:35 -0300

    sera que agr foi

--- src/circle.js
+++ src/circle.js
@@ -23,9 +23,12 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: parseFloat((PI * radius * radius).toFixed(2)),
-    circumference: parseFloat((PI * radius * 2).toFixed(2)),
+    area: PI * radius * radius,
+    circumference: 2 * PI * radius,
   };
 };
-console.log(circle(2));
+console.log(circle(3));
 module.exports = circle;
+
+// area: parseFloat((PI * radius * radius).toFixed(2)),
+// circumference: parseFloat((PI * radius * 2).toFixed(2)),
