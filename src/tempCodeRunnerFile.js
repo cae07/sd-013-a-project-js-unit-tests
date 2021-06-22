5e3f3b4f44b36eb884b6c8785adb6c3f336bd8de
ivandosss <ivanndosssilva9389@gmail.com>
2021-06-22 17:12:58 -0300

    1 - Implemente a função

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+
+const average = (array) => {
+  let output = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] === 'string') {
+      return undefined;
+    }
+    output += array[i];
+  }
+  return Math.round(output / array.length);
+};
