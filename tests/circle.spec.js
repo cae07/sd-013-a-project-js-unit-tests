56db973f8263d1d9ca77c7595d27857c9eddab83
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-22 16:25:18 -0300

    desafio 2 feito

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,8 +28,10 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(circle(),'object');
     // Teste se o objeto retornado tem 3 entradas.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(),undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
