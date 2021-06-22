56f09706400c579c46291cdd5643bf0197ec0ad9
Julia Baptista <baptista.julia@yahoo.com.br>
2021-06-22 18:55:52 -0300

    Ajustes no lint questao 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,13 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
+const createStudent = (nome, feedback) => {
   return {
     name: nome,
-    feedback: function() {
-      return 'Eita pessoa boa!';
-    }
-  }
+    feedback: () => 'Eita pessoa boa!',
+  };
 };
 
 module.exports = createStudent;
