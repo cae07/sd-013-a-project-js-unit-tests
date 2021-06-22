54c798b4dd1ba2c56019bc2d990659241e34b25c
FelipeNevess <felipe012neves@gmail.com>
2021-06-22 20:31:24 -0300

    Desafio 5 finalizado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const a = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return a;
+};
 
 module.exports = createStudent;
