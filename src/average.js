4cc81b4bb521dee123bdc92b31a8d7a67d206625
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-06-22 16:29:27 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,7 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-sdasd
-
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+  }
+  let media = Math.round(sum / array.length);
+  return media;
+};
 module.exports = average;
