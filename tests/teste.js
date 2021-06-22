61ae3d1aa3dbd8443ef752ba6caf17aa6526d7c5
Ricardo-Sousa <rwmsousa@gmail.com>
2021-06-22 17:39:57 -0300

    requisito 4

---
+++ tests/teste.js
@@ -0,0 +1,11 @@
+const circle = (radius) => {
+  const PI = 3.14;
+  if (!radius) { return undefined; }
+  return {
+    radius,
+    area: PI * radius * radius,
+    circumference: 2 * PI * radius,
+  };
+};
+
+console.log(typeof circle());
