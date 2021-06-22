3665a07f8913e246b6c37199e15218a4b3cdcd32
V. Dionysio <vini.dionysio@gmail.com>
2021-06-22 14:36:16 -0300

    Solving requirement 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,14 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (arr) => {
+  let total = 0;
+  if (arr.length === 0) return undefined;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof (arr[i]) !== 'number') return undefined;
+    total += arr[i];
+  }
+  return Math.round(total / arr.length);
 };
 
 module.exports = average;
