857bcb9c680f62533af96e8e2f4735e89fccbf4c
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-06-22 19:55:21 -0300

    correçao de erros do lint requisito 1

--- src/average.js
+++ src/average.js
@@ -12,21 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => { 
-  const average = (array) => {
+// const average = () => {};
+const average = (array) => {
     let soma = 0;
     if (array.lenght === 0) {
       return undefined;
     }
     for (let index = 0; index < array.lenght; index += 1) {
-      if (typeof array[index] ==='number') {
-        soma += array[index]
-      } else {
+      if (typeof array[index] !== 'number') {
         return undefined;
       }
+      soma += (array[index]);
     }
-    return Math.round (soma / array.lenght);
+    const output = (soma / array.lenght);
+    return Math.round(output);
   };
-average ([]);
 
 module.exports = average;
