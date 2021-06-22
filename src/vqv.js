e01949b2f0af2e514773f745e5afe4a8c88ca66e
Murilo Rainho <murilorainho01@gmail.com>
2021-06-22 19:06:31 -0300

    realizando requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,14 @@
 */
 
 const vqv = (nome, idade) => {
-  
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  const frase = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return frase;
 };
 
 module.exports = vqv;
