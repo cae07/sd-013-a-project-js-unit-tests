bf0daff44b42d82c5866986ef852ca64c65c8816
DouglasDrozda <lowdouglas9712@gmail.com>
2021-06-22 15:41:59 -0300

    feito requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-  
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  let count = 0;
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number'){
+      return undefined;
+    }
+    count += array[index];
+  }
+  return Math.round(count / array.length)
+};
+
+console.log(average([1, 1]))
 module.exports = average;
