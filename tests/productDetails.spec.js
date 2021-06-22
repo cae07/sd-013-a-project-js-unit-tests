0efa7a5d239cb6a202cfcf44181ca09468cea1cf
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-06-22 14:37:06 -0300

    resolve requisitos 8/9

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,41 +1,12 @@
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
-/*
-  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
-
-  Parâmetros:
-  - Uma string;
-  - Uma string;
-
-  Comportamento:
-  productDetails('Alcool gel', 'Máscara') // Retorna:
-  [
-    {
-      name: 'Alcool gel'
-      details: {
-        productId: 'Alcool gel123'
-      }
-    },
-    {
-      name: 'Máscara'
-      details: {
-        productId: 'Máscara123'
-      }
-    }
-  ]
-
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
-*/
-
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // Teste que os dois productIds terminam com 123.
+    assert.ok(Array.isArray(productDetails()));
+    assert.strictEqual(productDetails().length, 2);
+    assert.strictEqual(productDetails().every(e => typeof e === 'object'), true);
+    assert.ok(productDetails('a', 'b')[0] !== productDetails('a', 'b')[1]);
+    assert.ok(productDetails().every(e => e.details.productId.endsWith(123)));
   });
 });
