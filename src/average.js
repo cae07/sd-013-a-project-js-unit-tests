6f39f9c0e055510d1df66d783499cf75d42bfcc8
Ricardo-Sousa <rwmsousa@gmail.com>
2021-06-22 15:55:40 -0300

    primeiro requisito

--- src/average.js
+++ src/average.js
@@ -11,7 +11,18 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
- 
-const average = () => {};
+
+const average = (array) => {
+  let sumArray = 0;
+
+  if (array.length === 0) { return undefined; }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'string') {
+      return undefined;
+    }
+    sumArray += array[index];
+  }
+  return Math.round(sumArray / array.length);
+};
 
 module.exports = average;
