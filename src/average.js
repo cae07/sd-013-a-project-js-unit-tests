156797998b818e453063cc893642db2cfbefab0c
Fernando <nando_filipe@hotmail.com>
2021-06-22 18:31:14 -0300

    requisito 2

--- src/average.js
+++ src/average.js
@@ -14,15 +14,15 @@
 
 const average = (array) => {
  let soma = 0;
- if (array.length === 0) {
-  return undefined;
- } 
  for (let i = 0; i < array.length; i += 1) {
-  if (typeof array[i] !== 'number') {
-    return undefined;
+   if (typeof array[i] !== 'number') {
+     return undefined;
+    }
+    soma += array[i];
   }
-   soma += array[i];
- }
+  if (array.length === 0) {
+   return undefined;
+  } 
  let media = soma / array.length;
  return Math.round(media);
 };
