2441b7e9a7a145c0e027591f3c038452625e87c9
Jéssica Queiroz <gnr0034@gmail.com>
2021-06-22 16:26:07 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,20 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome == null || idade == null) {
+    return undefined;
+  } if (typeof nome === 'string' && typeof idade === 'number') {
+    let frase = (
+`Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+);
+    return frase;
+  }
+};
+
+vqv('Jéssica', 20);
 
 module.exports = vqv;
