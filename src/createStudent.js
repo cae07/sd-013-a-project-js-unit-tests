dfcb2a4ee8cea05691c486d5169017f2d8493e0b
Rafael Janovicci <rjanovicci@hotmail.com>
2021-06-22 20:25:34 -0300

    5 - createstudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const objEstudante = {
+    name: nome,
+    feedback: () => `Eita pessoa boa!`
+  };
+  return objEstudante;
+};
 
 module.exports = createStudent;
