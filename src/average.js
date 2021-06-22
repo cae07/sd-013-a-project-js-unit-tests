a094555170b0055febaaa37a1b7e781c0ff29c7b
Cassyel <cassyel1967@gmail.com>
2021-06-22 16:12:08 -0300

    Realiza Requisitos 1 e 2

--- src/average.js
+++ src/average.js
@@ -10,8 +10,21 @@
     - average([2, 2]) // Retorno: 2;
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
-*/ 
+*/
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i += 1) {
+    if ((typeof (array[i]) !== 'number') || (array.length === 0)) {
+      return undefined;
+    }
+    sum += array[i];
+  } if (array.length === 0) {
+    return undefined;
+  }
+  const functionAvarage = sum / array.length;
+  return Math.round(functionAvarage);
+};
 
 module.exports = average;
