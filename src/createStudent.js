be2b3c17fc868784039e0ef6acfde26df3062bb7
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-06-22 18:41:10 -0300

    Adiciona requisito 3

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => ({
+    name,
+    feedback: (phrase) => 'Eita pessoa boa!',
+  });
 
 module.exports = createStudent;
