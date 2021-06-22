816827623c75b9c0c20441f300bdae5ca3a7f1fb
Rafael Victor <rafavictorgs@gmail.com>
2021-06-22 18:19:22 -0300

    Resolve o requisito 3 do Projeto Unit Tests

---
+++ tests/testagem.js
@@ -0,0 +1,10 @@
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+console.log(vqv());
