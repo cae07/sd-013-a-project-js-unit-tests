42984ccec7f2f2a4eaa87bb99e4a602bcdd35caa
Cassyel <cassyel1967@gmail.com>
2021-06-22 18:50:16 -0300

    Realiza requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,27 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('param1', 'param2')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('param1', 'param2').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (productDetails('param1', 'param2')), 'object')
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails('param1', 'param2')[0] && [1], true);
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails('param1', 'param2')[0].details.productId.endsWith('123'), productDetails('param1', 'param2')[1].details.productId.endsWith('123'), true);
   });
 });
+
+/*
+1 - O primeiro teste faz a validação se o retorno da função é um array, utilizando o método 'isArray()', que retorna true ou false,
+e colocamos no 'expected' 'true';
+2 - O segundo teste faz a validação se o array retornado possiu dois itens dentro, fazemos isso acessando a propriedade 'length'
+e colocamos no 'expected' '2';
+3 - O terceiro teste faz a validação se os itens dentro desse array são objetos, utilizamento o metodo 'typeof()';
+4 - O quarto teste faz a validação se os itens são diferentes, utilizamento o metodo 'notStrictEqual', e passando os indices '0' e '1';
+5 - O quarto teste faz a validação se os valores dentro da chave 'productId' terminam com '123', utilizando o metodo 'endsWith()'
+e colocamos no 'expected' 'true';
+*/
