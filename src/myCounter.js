1fa255563df67857a0cf1dfb242845ed19022ab7
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-06-22 19:04:40 -0300

    Requisito 8 Arrumando Bug - ok

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,14 +17,14 @@
 
 const myCounter = () => {
   var myArray = [];
-  let counter = 0;
-  for (counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
 };
 
+console.log(myCounter())
 module.exports = myCounter;
