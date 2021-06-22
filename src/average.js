fa87aec300eec5087b68c8820d910ade23c40038
JVictor Baptista <dixxbaptista@gmail.com>
2021-06-22 14:56:39 -0300

    ex 1 feito

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let total = 0;
+  
+  if (array.length === 0){
+    return undefined
+  }
+  for (let i of array) {
+    if (typeof i === "string") {
+      return undefined
+    }
+    total += i;
+  }
+  total /= array.length;
+  let arredondado = Math.round(total)
+  return (arredondado);
+};
 
 module.exports = average;
