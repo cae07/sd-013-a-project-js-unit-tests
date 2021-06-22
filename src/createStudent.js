cb1def09481480a2dacced1a81c8c5345f28a1f9
Lanai Conceição <conceicao.lanai@gmail.com>
2021-06-22 18:08:07 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const objeto = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return objeto;
+};
+
+console.log(createStudent('lanai'));
 
 module.exports = createStudent;
