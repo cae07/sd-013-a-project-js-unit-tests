1f870012efd73887b1f78125236e4f955c7a3629
Cassyel <cassyel1967@gmail.com>
2021-06-22 20:00:12 -0300

    Realiza requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,19 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (num1, num2) => (num1 + num2),
+  sub: (num1, num2) => (num1 - num2),
+  mult: (num1, num2) => (num1 * num2),
+  div: (num1, num2) => Math.floor(num1 / num2),
+};
+
+/*
+1 - Primeiro adicionamos a chave 'add' que faz a soma dos parametros através da arrow function;
+2 - Adicionamos a chave 'sub' que faz a subtração dos parametros através da arrow function;
+3 - Adicionamos a chave 'mult' que faz a multiplicação dos parametros através da arrow function;
+4 - Adicionamos a chave 'div' que faz a divisão dos parametros através da arrow function, utilizando a propriedade 'Math.floor'
+para arredondar o numero para baixo
+*/
 
 module.exports = calculator;
