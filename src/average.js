9f71b8a43944e7ad79e844cd0cb376a8a28f6b9f
FelipeNevess <felipe012neves@gmail.com>
2021-06-22 16:21:28 -0300

    Desafio 1 finalizado

--- src/average.js
+++ src/average.js
@@ -12,8 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let soma = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    soma += array[i];
+    if (typeof array[i] === 'string') {
+      return undefined;
+    }
+  }
+
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  return Math.round(soma / array.length);
 };
 
 module.exports = average;
