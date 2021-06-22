543d4bfee228b877b3847759cb8c8f8b3a78733f
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-06-22 17:02:15 -0300

    Resolve requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const object = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+
+  return object;
+};
 
 module.exports = createStudent;
