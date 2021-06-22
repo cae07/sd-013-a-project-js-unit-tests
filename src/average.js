ab406db9d67bb39f39ad3d11f814e79ffb2968b8
Thiago <thiagoc.carboneri@gmail.com>
2021-06-22 17:27:37 -0300

    corrigindo erros lint

--- src/average.js
+++ src/average.js
@@ -1,26 +1,21 @@
-
 const average = (array) => {
-
   for (let i = 0; i < array.length; i += 1) {
     const element = array[i];
-    if (typeof element === "string") {
+    if (typeof element === 'string') {
       return undefined;
     }
   }
   if (array.length === 0) {
     return undefined;
-  } else {
-    let sum = 0;
-    let average = 0;
-    for (let i = 0; i < array.length; i += 1) {
-      const element = array[i];
-      sum += element;
-    }
-    average = Math.round((sum / array.length));
-    return average;
-  };
-}
-
-console.log(average([]));
+  }
+  let sum = 0;
+  let result = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    const element = array[i];
+    sum += element;
+  }
+  result = Math.round((sum / array.length));
+  return result;
+};
 
 module.exports = average;
--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  obj = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!'
+  }
+};
 
 module.exports = createStudent;
