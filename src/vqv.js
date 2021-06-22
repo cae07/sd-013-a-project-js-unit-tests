bb37f6a25f3e1d5b189c28ed806cf69c619c4c27
fgosuen <fgosuendacosta@gmail.com>
2021-06-22 13:47:37 -0300

    Adição resolução vqv.js

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,12 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
-
+const vqv = (nome, idade) => {
+  if (!nome || !idade) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\n +
+  trabalho na Trybe e mando muito em programação!\n#VQV!`;
+};
+console.log(vqv('Fernando', 40));
 module.exports = vqv;
