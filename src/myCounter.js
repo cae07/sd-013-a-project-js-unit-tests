d5a98d7d997e2fdee16d6c514a7f7eb440e6051a
marcos <marc0sdurval20@gmail.com>
2021-06-22 18:38:53 -0300

    myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var j = 2; j <= 3; j += 1) {
+      myArray.push(j);
     }
   }
   return myArray;
