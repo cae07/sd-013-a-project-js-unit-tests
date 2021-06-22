c96e25439f007a993497d460efca5ee76df02387
Murilo Ribeiro <murilorsv14@gmail.com>
2021-06-22 19:16:59 -0300

    requisito calculator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -5,8 +5,8 @@
     - div;
     - sub.
   Para cada uma delas atribua uma função que realiza a respectiva operação.
-  A função deve receber dois inteiros e retornar um inteiro.
-  Os resultados das divisões devem sempre ser arredondados para baixo.
+  A função deve receber dois inteiros e retornar um inteiro. parseINT
+  Os resultados das divisões devem sempre ser arredondados para baixo. mathfloor
 
   Faça as funções com arrow functions!
 
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (a, b) => parseInt(a, 10) + parseInt(b, 10),
+  mult: (a, b) => parseInt(a, 10) * parseInt(b, 10),
+  div: (a, b) => Math.floor(parseInt(a, 10) / parseInt(b, 10)),
+  sub: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+};
 
 module.exports = calculator;
