7aff2d87b68ee2aebd7e8ba6016f66d160efdb88
Fernando <nando_filipe@hotmail.com>
2021-06-22 17:33:04 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+ let soma = 0;
+ if (array.length === 0) {
+  return undefined;
+ } 
+ for (let i = 0; i < array.length; i += 1) {
+  if (typeof array[i] !== 'number') {
+    return undefined;
+  }
+   soma += array[i];
+ }
+ let media = soma / array.length;
+ return Math.round(media);
+};
 
 module.exports = average;
