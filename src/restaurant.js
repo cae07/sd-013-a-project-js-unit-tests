f0ed1c8de094bfab6c1e05f27d2eb2d19bc86ff3
Icaro Ferreira <icaro.iky@gmail.com>
2021-06-22 19:24:07 -0300

    Ajuste lint

--- src/restaurant.js
+++ src/restaurant.js
@@ -80,7 +80,7 @@
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
 const createMenu = (obj) => {
-  const objCreated = {fetchMenu: () => obj};
+  const objCreated = { fetchMenu: () => obj };
   return objCreated;
 };
 
