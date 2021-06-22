20cc4e6d908c9fa34ec3d23dde77720e51b958b3
Diego Vilela <vilelaphp@gmail.com>
2021-06-22 18:39:31 -0300

    Implement productDetails tests

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { prototype } = require('events');
 const productDetails = require('../src/productDetails');
 
 /*
@@ -30,12 +31,22 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.prototype.isPrototypeOf(productDetails('Book', 'Headphone')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Book', 'Headphone').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(
+      productDetails('Book', 'Headphone').filter((i) => typeof (i) !== 'object'),
+      []
+    )
     // Teste que os dois objetos são diferentes entre si.
+    const products = productDetails('Book', 'Headphone');
+    assert.notDeepStrictEqual(products[0], products[1]);
     // Teste que os dois productIds terminam com 123.
+    const p0 = products[0].details.productId.endsWith('123');
+    const p1 = products[1].details.productId.endsWith('123');
+    assert.strictEqual(p0 && p0, true);
   });
 });
