942773f5f54554f1bc1d88bb8a844d3105473cc9
Igor Fellows <igor.fpp96@gmail.com>
2021-06-22 18:09:22 -0300

    Corrige erros do lint da funçao average

--- src/average.js
+++ src/average.js
@@ -14,18 +14,18 @@
 
 const average = (array) => {
   let sum = 0;
-  if (array.length === 0){
+  if (array.length === 0) {
     return undefined;
   }
 
-  for(let number of array) {
+  for (let number of array) {
     if (typeof number !== 'number') {
       return undefined;
     }
     sum += number;
   }
 
-  return Math.round(sum / array.length)
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
