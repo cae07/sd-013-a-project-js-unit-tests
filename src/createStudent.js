dcb1c34ad06c87099a4c7497e973db6e85ca7837
Caê <eduardokae01@hotmail.com>
2021-06-22 20:54:30 -0300

    requisito 5 Unit Tests

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const estudante {
+    name: nome,
+  }
+    estudante.feedback = `Eita pessoa boa!`
+};
 
 module.exports = createStudent;
