55261fbb2d604865bcce062fb21db839979e221e
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-06-22 20:31:11 -0300

    Corrigi erros do lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,10 +17,10 @@
 
 const myCounter = () => {
   let myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  for (let i = 0; i <= 3; i += 1) {
+    myArray.push(i);
+    for (let j = 2; j <= 3; j += 1) {
+      myArray.push(j);
     }
   }
   return myArray;
