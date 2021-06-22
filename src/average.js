25ca31c67b3aa9d1fbbc6eafc7ac56b8b8cf3ac4
marcos <marc0sdurval20@gmail.com>
2021-06-22 16:22:27 -0300

    vqv conluido

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (t) => {
+  let cont = 0;
+  let string = 0;
+  for (let key in t) {
+    if (typeof (t[key]) === 'number') {
+      cont += t[key];
+    } else {
+      string += 1;
+    }
+  } if (string === 0) {
+    return Math.round(cont / t.length);
+  } 
+    return undefined;
+};
 
 module.exports = average;
