21e506cc867914fe7a91d969d08d434887b1a00a
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-22 16:51:08 -0300

    my Counter prontinho

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,9 +19,8 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    // eslint-disable-next-line no-shadow
-    for (counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
