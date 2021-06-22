4f746f5937a72074c929877aa21ab54544c6fe5c
Julia Baptista <baptista.julia@yahoo.com.br>
2021-06-22 20:41:41 -0300

    7. Implemente a função objCalculator

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,25 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const output = productDetails('Alcool gel', 'Máscara');
+    assert.deepStrictEqual(Array.isArray(output), true);
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(output.length, 2);
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual([ typeof(output[0]), typeof(output[1])], ['object', 'object']);
+
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(output[0], output[1]);
+
     // Teste que os dois productIds terminam com 123.
+    const ids = [output[0].details.productId, output[1].details.productId];
+    // assert.match([ids[0], ids[1]], [/123$/, /123$/]);
+    assert.match(ids[0], /123$/);
+    assert.match(ids[1], /123$/);
   });
 });
