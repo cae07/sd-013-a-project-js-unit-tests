c800f989543b91a5cb7c01b6c751b3156b65ca22
jp Viegas <jp.viegas@hotmail.com>
2021-06-22 19:16:56 -0300

    finalizando requisito 4

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name, feedback) => {
+  console.log(name, feedback);
+};
 
 module.exports = createStudent;
