2b86683c8a915041bdf4bcf59c6bedd55fb4d0f9
Gustavo Pia <piagja@gmail.com>
2021-06-22 18:57:48 -0300

    Atendeu ao requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -34,8 +34,16 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     assert.strictEqual(Object.entries(circle(1)).length, 3);
 
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
+
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2).circumference, 12.56);
+
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(parseFloat(circle(3).area).toPrecision(4), "28.26");
+
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const pi = 3.14 * 3 * 3;
+    assert.deepStrictEqual(circle(3), { radius: 3, area: pi, circumference: 18.84 });
   });
 });
