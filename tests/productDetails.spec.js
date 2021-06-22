1221e141c6ec04d77781052991cf2a3461236259
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-06-22 20:08:20 -0300

    resolvendo funcao productDetails.js

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,22 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const res1 = productDetails('Alcool gel', 'Máscara')[0]
+    const res2 = productDetails('Alcool gel', 'Máscara')[1]
+
+    assert.strictEqual(typeof res1 === 'object' && typeof res2 === 'object', true);
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(res1 !== res2, true)
     // Teste que os dois productIds terminam com 123.
+    const id1 = res1.details.productId
+    const id2 = res2.details.productId
+    /* Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/slice */
+    assert.strictEqual(id1.slice(id1.length - 3) === '123' && id2.slice(id2.length - 3) === '123', true)
   });
 });
