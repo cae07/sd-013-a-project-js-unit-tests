b5110af7416edb49bfe18a6145bfda63b1bbe9fb
Debora Teodorico <debora.steodorico@gmail.com>
2021-06-22 17:16:02 -0300

    Adicionado a questao 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,10 +18,21 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
-    assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    const resultadoReal = numbers([1, 2, 3, 4, 5])
+    const resultadoEsperado = true
+    assert.strictEqual(resultadoReal, resultadoEsperado);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    const resultadoReal2 = numbers([1, 2, '3', 4, 5]);
+    const resultadoEsperado2 = false;
+    assert.strictEqual(resultadoReal2, resultadoEsperado2);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    const resultadoReal3 = numbers([1, 'a', 3]);
+    const resultadoEsperado3 = false;
+    assert.strictEqual(resultadoReal3, resultadoEsperado3);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    const resultadoReal4 = numbers(['']);
+    const resultadoEsperado4 = false;
+    assert.strictEqual(resultadoReal4, resultadoEsperado4);
   });
 });
