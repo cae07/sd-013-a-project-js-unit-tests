2076497cb1ba145e0022fef442faca0677fc73e6
Marcelo Alessandro <marceloouropreto@gmail.com>
2021-06-23 11:51:30 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-  const assert = require('assert');
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1){
+    if (typeof array[i] !== 'number') {
+    return undefined;
+  } 
+    sum += array[i];
+  }
+  return Math.round(sum / array.length);
+};
+
 module.exports = average;
