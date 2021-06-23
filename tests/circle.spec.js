af085973f6932050def8f7ee466d3c8e430d15b0
icaroMelo <exemployt@gmail.com>
2021-06-23 18:31:28 -0300

    Correção 8 e circle.spec.js

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,9 +28,9 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.strictEqual(typeof circle(4), 'object');
+    assert.strictEqual(typeof circle(Math.random), 'object');
     // Teste se o objeto retornado tem 3 entradas
-    assert.strictEqual(Object.keys(circle(4)).length, 3);
+    assert.strictEqual(Object.keys(circle(Math.random)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
