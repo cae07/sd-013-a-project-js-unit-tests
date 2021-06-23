c2f48f1299bad5d167f909d16e6198a240d44a15
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-06-23 12:02:46 -0300

    Resolve o requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,14 +28,24 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+// Requisito 6
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    const product = productDetails('caneta', 'lápis');
     // Teste que o retorno da função é um array.
-    // assert.strictEqual(Array.isArray(productDetails('jaquelline', 'silva')), true);
+    assert.strictEqual(Array.isArray(product), true);
     // Teste que o array retornado pela função contém dois itens dentro.
-    // assert.
+    assert.strictEqual(product.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (product[0]), 'object');
+    assert.strictEqual(typeof (product[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(product[0], product[1]);
     // Teste que os dois productIds terminam com 123.
+    const product0 = product[0].details.productId;
+    const product1 = product[1].details.productId;
+
+    assert.strictEqual(product0.substr(product0.length - 3), '123');
+    assert.strictEqual(product1.substr(product1.length - 3), '123');
   });
 });
