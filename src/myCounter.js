5188d78b0c37b37323ba5af7de405f0b21397ba6
Thiago <thiagoc.carboneri@gmail.com>
2021-06-22 22:14:00 -0300

    refatorando requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,10 +17,10 @@
 
 const myCounter = () => {
   const myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-     for (counter = 2; counter <= 3; counter += 1) {
-       myArray.push(counter);
+  for (let counter1 = 0; counter <= 3; counter += 1) {
+    myArray.push(counter1);
+     for (let counter2 = 2; counter <= 3; counter += 1) {
+       myArray.push(counter2);
      }
   }
   console.log(myArray);
