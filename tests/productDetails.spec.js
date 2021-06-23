5bcf4081bbb3524545d5efca652b1f4d44957e11
turtore <brunosap@gmail.com>
2021-06-22 23:02:32 -0300

    completos requisitos questao 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,14 +33,14 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    // assert.strictEqual(typeof productDetails, 'function');
+    assert.deepStrictEqual(Array.isArray(productDetails('Alcool gel','Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails.length, 2)
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof productDetails[0], 'object')
+    assert.strictEqual(typeof productDetails('Alcool gel','Máscara')[0], 'object')
     // Teste que os dois objetos são diferentes entre si.
     assert.deepStrictEqual(productDetails[0],productDetails[1])
     // Teste que os dois productIds terminam com 123.
-    assert.strictEqual(productDetails[0[2]],productDetails[1[2]])
+    assert.strictEqual(productDetails('Alcool gel','Máscara')[0].details.productId.slice(-3), '123')
   });
 });
