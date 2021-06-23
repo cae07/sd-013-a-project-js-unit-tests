3f0256a6b19b69a39f3bfbfd8edf2ed098eeb2e0
Filipe <filipecandido123@hotmail.com>
2021-06-22 22:06:15 -0300

    requisito 6 feito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -41,6 +41,8 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que os dois objetos são diferentes entre si.
     assert.notDeepStrictEqual(typeof(productDetails('oi','oi')))
     // Teste que os dois productIds terminam com 123.
-    assert.deepStrictEqual(productDetails('x-ratão','coca')[0].details.productId.endsWith('123'), true);
+    // assert.deepStrictEqual(productDetails('x-ratão','coca')[0].details.productId.endsWith('123'), true);
+
+    assert.strictEqual(productDetails('x-ratão')[0].details.productId.endsWith('123'), productDetails('coca')[1].details.productId.endsWith('123')); 
   });
 });
