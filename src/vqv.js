1a411006d16e9d16f7ce906e0640ee812df8f0d0
Ygor <ygormaiaa1@gmail.com>
2021-06-23 10:17:20 -0300

    alterando lint

--- src/vqv.js
+++ src/vqv.js
@@ -17,15 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-
   if (typeof nome === 'string' && typeof idade === 'number') {
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
-  } else {
+#VQV!`;
+}
   return undefined;
-  }
 };
 
 module.exports = vqv;
