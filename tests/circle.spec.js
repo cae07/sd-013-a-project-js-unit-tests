834f033a1558f2d85f48cb6f9d30d98817645b00
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-06-23 09:18:38 -0300

    Corrige requisito circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,17 +28,17 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert(circle(1), {});
+    assert.strictEqual(typeof (circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert(circle(7), {radius: 7, area: 153.86, circumference: 43.96});
+    assert.strictEqual(Object.keys(circle(7)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert(circle(2), 12.56);
+    assert.strictEqual(((parseFloat((circle(2).circumference).toPrecision(4)))), 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert(circle(3), 28.26)
+    assert.strictEqual(((parseFloat((circle(3).area).toPrecision(4)))), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert(circle(3), {radius: 3, area: 28.26, circumference: 18.84});
+    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
     
   });
 });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,16 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert(productDetails('Alcool gel', 'Máscara'), []);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert(['Alcool gel', 'Máscara'], 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert(['Álccol gel', 'Máscara'], ({}, {}));
     // Teste que os dois objetos são diferentes entre si.
+    assert(productDetails('Álcool Gel', 'Máscara'));
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(typeof (Object.entries(productDetails('Alcool gel', 'Máscara'))),'object'); 
   });
 });
