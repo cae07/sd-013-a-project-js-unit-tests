dfebcf43ef4b8cbbb04563b265c3c92dc372cbfc
turtore <brunosap@gmail.com>
2021-06-22 22:48:31 -0300

    adicionado requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,14 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
 };
+// let teste1 = myCounter()
+// console.log(teste1)
+// Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 
 module.exports = myCounter;
