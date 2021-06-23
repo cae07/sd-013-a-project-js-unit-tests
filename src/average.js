8eae39a940949d5ec89bde23b717526e94e83020
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-06-22 22:07:24 -0300

    correçoes ortograficas

--- src/average.js
+++ src/average.js
@@ -15,16 +15,16 @@
 // const average = () => {};
 const average = (array) => {
     let soma = 0;
-    if (array.lenght === 0) {
+    if (array.length === 0) {
       return undefined;
     }
-    for (let index = 0; index < array.lenght; index += 1) {
+    for (let index = 0; index < array.length; index += 1) {
       if (typeof (array[index]) !== 'number') {
         return undefined;
       }
       soma += array[index];
     }
-    return Math.round(soma / array.lenght);
+    return Math.round(soma / array.length);
   };
 
 module.exports = average;
