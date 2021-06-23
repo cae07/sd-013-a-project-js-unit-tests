bf17cdd8503730d49e2fea52fe1c777a9be74303
Gustavo Mauricio <gmauricio207@gmail.com>
2021-06-23 15:14:24 -0300

    Iniciando requisito 9

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-const productDetails = require('../src/productDetails');
+const assert = require("assert");
+const productDetails = require("../src/productDetails");
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -28,20 +28,31 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+describe("6 - Implemente os casos de teste para a função `productDetails`", () => {
+  it("Verifica se a função `productDetails` tem o comportamento esperado", () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     assert.deepStrictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual(Object.keys(productDetails('Alcool gel', 'Máscara')).length,2)
+    assert.strictEqual(
+      Object.keys(productDetails("Alcool gel", "Máscara")).length,
+      2
+    );
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof Object.values(productDetails),'object')
+    assert.strictEqual(typeof Object.values(productDetails), "object");
     // Teste que os dois objetos são diferentes entre si.
-    assert.notStrictEqual(Object.values(productDetails('a','b')[0]) !== Object.values(productDetails('a','b')[1]),false)
+    assert.notStrictEqual(
+      Object.values(productDetails("a", "b")[0]) !==
+        Object.values(productDetails("a", "b")[1]),
+      false
+    );
     // Teste que os dois productIds terminam com 123.
 
-    assert.strictEqual((productDetails('a','b')[0]).details.productId.includes('123') && (productDetails('a','b')[1]).details.productId.includes('123'), true );
+    assert.strictEqual(
+      productDetails("a", "b")[0].details.productId.includes("123") &&
+        productDetails("a", "b")[1].details.productId.includes("123"),
+      true
+    );
   });
 });
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -48,23 +48,30 @@ const createMenu = require('../src/restaurant');
 
 describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
-    // ```
+    // ```    
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
+    assert.deepStrictEqual(Object.keys(createMenu)[0],'fetchMenu');
+  
+
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
     // const objetoRetornado = createMenu({ food: {}, drink: {} });
     // objetoRetornado.fetchMenu() // Retorno: { food: {}, drink: {}}
     // ```
+    const objetoRetornado = createMenu({ food: {}, drink: {}});
+    assert.strictEqual(objetoRetornado.fetchMenu(), { food: {}, drink: {}})
+
     // TESTE 3: Verifique que o menu passado pra função createMenu é identico ao menu recuperado pela função 'objetoRetornado.fetchMenu'
     // ```
     // const objetoRetornado = createMenu(objetoQualquer);
     // objetoRetornado.fetchMenu() // Retorno: objetoQualquer
     // ```
+    assert.strictEqual(createMenu({food: {}, drink: {}}), [food, drink])
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 4: Verifique que 'objetoRetornado.consumption', após a criação do menu, retorna um array vazio.
