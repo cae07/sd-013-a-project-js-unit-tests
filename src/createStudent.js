05e6d9b8b9a5688195c919f2a43b5fc0d7147197
Ygor <ygormaiaa1@gmail.com>
2021-06-23 16:38:15 -0300

    requisito 05 ok

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,11 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+  const estudante = { 
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+};
 module.exports = createStudent;
