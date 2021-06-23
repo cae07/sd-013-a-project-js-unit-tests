64bbad8c9a531cf38a74cfaa33c055f3d3667a28
Filipe <filipecandido123@hotmail.com>
2021-06-22 21:04:15 -0300

    arrumando lint

--- src/numbers.js
+++ src/numbers.js
@@ -2,7 +2,7 @@
   A função numbers recebe um array (tamanho variável),
   retornando true se todos os parâmetros forem do tipo 'number' e false caso contrário.
 
-  Parâmetros:
+  Parâmetros:pois
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
   Comportamento:
     - numbers([2, 3, 4]); // Retorna: true
