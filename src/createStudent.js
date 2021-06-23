df2ed71fa921931d9419b2ee153d54ede468fb47
Emídio Rebouças <emidioreb@gmail.com>
2021-06-23 11:57:36 -0300

    req 5 ok

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (getName) => {
+  const estudante={
+    name: `${getName}`,
+    feedback: function(){return 'Eita pessoa boa!'} 
+  }
+  return estudante
+}
 
 module.exports = createStudent;
