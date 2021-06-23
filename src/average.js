97321480ff30952c1c734442ba5473ebbd8bd696
icaroMelo <exemployt@gmail.com>
2021-06-23 14:17:50 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let x = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    } else {
+    x += array[i];
+  }
+  }
+  let average2 = Math.round(x / array.length);
+  return average2;
+};
 
 module.exports = average;
