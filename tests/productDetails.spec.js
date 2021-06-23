5ac3f2f7badd6d24e067df4d83fcfe919393ff15
Joao Santos <jvitor.spaula@gmail.com>
2021-06-23 16:48:40 -0300

    Requisito 8

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -32,19 +32,10 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     assert.fail()
-    // Teste que o retorno da função é um array.
-    
-    
+    // Teste que o retorno da função é um array. 
     // Teste que o array retornado pela função contém dois itens dentro.
-  
-    
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    
-    
     // Teste que os dois objetos são diferentes entre si.
-    
-
     // Teste que os dois productIds terminam com 123.
-    
   });
 });
