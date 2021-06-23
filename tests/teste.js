0851f1666d9b5da400ea535cf7b99aed6a8e4690
Ricardo-Sousa <rwmsousa@gmail.com>
2021-06-23 10:25:54 -0300

    requisito 8

--- tests/teste.js
+++ tests/teste.js
@@ -1,11 +1,12 @@
-const circle = (radius) => {
-  const PI = 3.14;
-  if (!radius) { return undefined; }
-  return {
-    radius,
-    area: PI * radius * radius,
-    circumference: 2 * PI * radius,
-  };
+const myCounter = () => {
+  var myArray = [];
+  for (var counter = 0; counter <= 3; counter += 1) {
+    myArray.push(counter);
+    for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
+    }
+  }
+  return myArray;
 };
 
-console.log(typeof circle());
+console.log(myCounter());
