811c650c25bb3bbf56e958509fcb3de98f02d833
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-23 14:14:00 -0300

    desafio 5 feito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+  const obj = {
+    name: nome,
+    feedback: () => 
+      'Eita pessoa boa!', 
+  };
+  return obj;
+};
+console.log(createStudent('Joao').feedback);
 module.exports = createStudent;
