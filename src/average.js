a5c450819dffb39d849bc41a42133018adfc2f5c
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-06-23 20:32:17 -0300

    Lint 1

--- src/average.js
+++ src/average.js
@@ -18,10 +18,8 @@ const average = (array) => {
 
   let soma = 0;
 
-  for (let i in array) {
-    if (typeof array[i] !== 'number') {
-      return undefined;
-    }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') return undefined;
     soma += array[i];
   }
 
