c3a087637e5e4a5477946a01512dcd73e4e1bd9c
Gabriel Lenz <lenzordmusic@gmail.com>
2021-06-24 08:47:16 -0400

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const studentInfo = {
+    name,
+    feedback: () => 'Eita pessoa boa!'
+  }
+  return studentInfo
+};
 
 module.exports = createStudent;
