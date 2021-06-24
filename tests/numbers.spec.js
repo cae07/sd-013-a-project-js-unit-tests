c21ea5e4851be191ace29f23cf8a22ece2d7eb54
Thiago <thiagoc.carboneri@gmail.com>
2021-06-24 19:49:23 -0300

    resolve merge conflicts

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -17,20 +17,7 @@ const numbers = require('../src/numbers');
 */
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-<<<<<<< HEAD
-  it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
-    //assert.fail();
-    assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
-    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
-    assert.deepStrictEqual(numbers([1, 'a', 5]), false);
-    assert.strictEqual(numbers([' ']), false);
-
-    
-
-=======
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-    assert.fail();
->>>>>>> 49272e38a6149bc7cc5b623b2385a558133b84bf
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
