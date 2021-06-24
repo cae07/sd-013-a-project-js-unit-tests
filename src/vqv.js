d5f45b545e69e6d3915a9ff391fa406111779480
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-06-23 22:08:07 -0300

    Lint

--- src/vqv.js
+++ src/vqv.js
@@ -3,7 +3,8 @@
 const vqv = (nome, idade) => {
   if (!nome || !idade) return undefined;
 
-  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\n`
+    + `trabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
