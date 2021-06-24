478f616ba0c506f6dccc902258d4d6fa5916422f
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-24 20:00:54 -0300

    commit de att

---
+++ src/dist/objCalculator.dev.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/*
+  Desenvolva um objeto calculator que possui quatro chaves:
+    - add;
+    - mult;
+    - div;
+    - sub.
+  Para cada uma delas atribua uma função que realiza a respectiva operação.
+  A função deve receber dois inteiros e retornar um inteiro.
+  Os resultados das divisões devem sempre ser arredondados para baixo.
+
+  Faça as funções com arrow functions!
+
+  Parâmetros:
+  - Um número inteiro;
+  - Um número inteiro;
+
+  Comportamento:
+  calculator.add(1, 1) // Retorno: 2;
+  calculator.div(3, 2) // Retorno: 1;
+*/
+var calculator = {
+  add: function add(num1, num2) {
+    return num1 + num2;
+  },
+  mult: function mult(num1, num2) {
+    return num1 * num2;
+  },
+  div: function div(num1, num2) {
+    return Math.floor(num1 / num2);
+  },
+  sub: function sub(num1, num2) {
+    return num1 - num2;
+  }
+};
+module.exports = calculator;
---
+++ tests/dist/productDetails.spec.dev.js
@@ -0,0 +1,51 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+var assert = require('assert');
+
+var productDetails = require('../src/productDetails');
+/*
+  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara') // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
+*/
+
+
+describe('6 - Implemente os casos de teste para a função `productDetails`', function () {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', function () {
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(Array.isArray(productDetails('tes1', 'tes2')), true); // Teste que o array retornado pela função contém dois itens dentro.
+
+    assert.strictEqual(productDetails('tes1', 'tes2').length, 2); // Teste que os dois itens dentro do array retornado pela função são objetos.
+
+    assert.strictEqual(_typeof(productDetails('tes1', 'tes2')[0] && productDetails('tes1', 'tes2')[1]), 'object'); // Teste que os dois objetos são diferentes entre si.
+
+    assert.notStrictEqual(productDetails('tes1', 'tes2')[0], productDetails('tes1', 'tes2')[1]); // Teste que os dois productIds terminam com 123.
+
+    assert.strictEqual(productDetails('alcool', 'gel')[0].details.productId.endsWith('123'), true);
+    assert.strictEqual(productDetails('alcool', 'gel')[1].details.productId.endsWith('123'), true);
+  });
+});
