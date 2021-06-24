2d0a59819ad10c2caf6b6182e643f68a4742ad35
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-06-23 22:04:57 -0300

    Requisito 4

--- src/vqv.js
+++ src/vqv.js
@@ -3,8 +3,7 @@
 const vqv = (nome, idade) => {
   if (!nome || !idade) return undefined;
 
-  console.log(`Oi, meu nome é ${nome}!\n Tenho ${idade} anos,
-  Trabalho na Trybe e mando muito em programação!\n #VQV`);
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
