80d674ef700b229553b2c668e38e01472b74af82
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-06-23 23:08:01 -0300

    teste 4 do req 9 - verfica se a chave consumption retorna array e vazio

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -88,8 +88,10 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // --------------------------------------------------------------------------------------
     // TESTE 4: Verifique que 'objetoRetornado.consumption', após a criação do menu, retorna um array vazio.
     // ```
-    // const objetoRetornado = createMenu(objetoQualquer);
-    // objetoRetornado.consumption // Retorno: []
+    const objetoRetornado4 = createMenu(objMenuzin);
+    // objetoRetornado4.consumption // Retorno: []
+    assert.strictEqual(Array.isArray(objetoRetornado4.consumption), true);
+    assert.strictEqual(objetoRetornado4.consumption.length === 0, true);
     // ```
     // Agora faça o PASSO 2 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
