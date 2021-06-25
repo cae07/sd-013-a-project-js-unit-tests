e597488a362b7381d242eff725ea451173ab38c8
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-06-25 00:56:03 -0400

    Requisito 6 e ajustes no Requisito 2

--- src/createStudent.js
+++ src/createStudent.js
@@ -24,3 +24,4 @@ const createStudent = (nome) => {
 };
 
 module.exports = createStudent;
+
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,23 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const myfunction = productDetails('Alcool gel', 'Máscara');
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof myfunction, 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(myfunction.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof myfunction[0], 'object');
+    assert.deepStrictEqual(typeof myfunction[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(Object.entries(myfunction[0]), Object.entries(myfunction[1]));
     // Teste que os dois productIds terminam com 123.
+    const item1 = myfunction[0].details.productId;
+    const item2 = myfunction[1].details.productId;
+    assert.deepStrictEqual(item1.substr(item1.length-3), '123');
+    assert.deepStrictEqual(item2.substr(item2.length-3), '123');
+
   });
 });
+
