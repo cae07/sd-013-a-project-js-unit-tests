9332d596052f38e903884352ef2ba0c2be78380f
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-06-25 16:12:22 -0300

    Resolve o requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,13 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (n1, n2) => n1 + n2, // Soma os dois numeros inteiros.
+  mult: (n1, n2) => n1 * n2, // Multiplica os dois numeros inteiros.
+  div: (n1, n2) => Math.floor(n1 / n2), // Divide e arredonda para baixo os dois numeros inteiros.
+  sub: (n1, n2) => n1 - n2, // Subtrai os dois numeros inteiros.
+};
+
+console.log(calculator.add(1, 2));
 
 module.exports = calculator;
