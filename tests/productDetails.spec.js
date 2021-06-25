2562e53e712877ac44f950446182fe34eef30788
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-25 19:33:47 -0300

    desafio 6 feito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,10 +37,10 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(Object.keys(productDetails()).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    //assert.strictEqual(typeOf(Object.values(productDetails())), 'object');
-
+    assert.strictEqual((typeof (productDetails())), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(productDetails()[0]===productDetails()[1], false);
     // Teste que os dois productIds terminam com 123.
-   // assert.strictEqual()
+    assert.strictEqual(productDetails().every((product) => product.details.productId.endsWith('123')), true); 
   });
 });
