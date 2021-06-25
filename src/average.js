9fe084895e5f7f55c895349ce1745df70df0dcc0
Antonio Martins <anmmartyns@gmail.com>
2021-06-24 22:02:38 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // teste
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let soma = 0;
+  let media = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    } else {
+      soma += array[index];
+    }
+  }
+  media = soma / array.length;
+  return Math.round(media);
 };
-
+console.log()
 module.exports = average;
