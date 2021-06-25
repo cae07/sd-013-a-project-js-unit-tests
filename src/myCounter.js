9f38add3cd2439fa855ee971ee30890bbf6fe0f6
Luiza Antiques <luiza.antiques@gmail.com>
2021-06-24 22:36:25 -0300

    iniciando requisito 9

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let index = 2; index <= 3; index += 1) {
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,6 +51,7 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
+    //assert.strictEqual(typeof (createMenu().fetchMenu), 'function');
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
