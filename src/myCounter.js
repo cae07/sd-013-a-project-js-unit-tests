5b1aded486069e2692b6733682d628c8981b5753
Thiago <thiagoc.carboneri@gmail.com>
2021-06-26 15:35:54 -0300

    resolve requisito 8 -counter is not defined

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,9 +17,9 @@
 
 const myCounter = () => {
   const myArray = [];
-  for (let counter1 = 0; counter <= 3; counter += 1) {
+  for (let counter1 = 0; counter1 <= 3; counter1 += 1) {
     myArray.push(counter1);
-     for (let counter2 = 2; counter <= 3; counter += 1) {
+     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
        myArray.push(counter2);
      }
   }
--- src/restaurant.js
+++ src/restaurant.js
@@ -79,6 +79,14 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, 
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = (obj) => {
+  objRetornado = {
+    fetchMenu: () => { 
+      return obj;
+    },
+  }
+};
 
 module.exports = createMenu;
+
+
