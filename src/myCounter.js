c8d02413afc32d5dc65d8a0133b8b0f130a58c2b
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-26 18:03:03 -0300

    desafio 8 feito

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let number = 2; number <= 3; number += 1) {
+      myArray.push(number);
     }
   }
   return myArray;
