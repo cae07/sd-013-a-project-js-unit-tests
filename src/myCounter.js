a27dfc66d4d68deb6aa378fba2395d4dda6195dd
Emídio Rebouças <emidioreb@gmail.com>
2021-06-26 20:43:21 -0300

    req 8 ok

--- src/myCounter.js
+++ src/myCounter.js
@@ -21,9 +21,31 @@ const myCounter = () => {
     myArray.push(counter);
     for (var counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
+      for (var counter = 3; counter < 4; counter += 1) {
+        myArray.push(counter);
+        for (var counter = 1; counter <= 3; counter += 1) {
+          myArray.push(counter);
+        }
+        for (var counter = 2; counter <3; counter += 1) {
+          myArray.push(counter);
+          for (var counter = 2; counter <3; counter += 1) {
+            myArray.push(counter);
+            for (var counter = 3; counter <4; counter += 1) {
+              myArray.push(counter);
+              for (var counter = 3; counter <4; counter += 1) {
+                myArray.push(counter);
+                for (var counter = 2; counter <=3; counter += 1) {
+                  myArray.push(counter);
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
+
