7e92a820cb8209a4b1ed03a1c45451d447e5e474
Julio Fallace <julioscoffee@gmail.com>
2021-06-26 00:46:41 -0300

    resolvendo requisito 5, lint ok

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const pessoa = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return pessoa;
+};
 
 module.exports = createStudent;
