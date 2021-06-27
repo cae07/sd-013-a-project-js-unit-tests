5362d9e9ef7589ece6c74cadcc0eae2d0a60f4a1
LuizFJP <luizfjportela@gmail.com>
2021-06-27 12:39:39 -0300

    requirement 07

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,10 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
-
+const calculator = {
+    add: (par1, par2) => par1 + par2,
+    mult: (par1, par2) => par1 * par2,
+    div: (par1, par2) => Math.floor(par1 / par2),
+    sub: (par1, par2) => par1 - par2,
+  };
 module.exports = calculator;
