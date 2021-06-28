76711dedd80a4632a484024ca28fad2cf15b7f8b
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-06-28 17:59:06 -0400

    Requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (ad1, ad2) => ad1 + ad2,
+  mult: (mult1, mult2) => mult1 * mult2,
+  div: (div1, div2) => Math.trunc(div1 / div2),
+  sub: (sub1, sub2) => sub1 - sub2,
+};
 
 module.exports = calculator;
