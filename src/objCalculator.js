f08511c2e940c145d3a6a50a671863d0b263dc44
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-06-27 23:27:27 -0300

    Adiciona requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (obj1, obj2) => (obj1 + obj2),
+  mult: (obj1, obj2) => (obj1 * obj2),
+  div: (obj1, obj2) => Math.trunc(obj1 / obj2),  //O método Math.trunc() retorna a parte inteira de um número, descartando suas casas decimais.
+  sub: (obj1, obj2) => (obj1 - obj2),
+};
 
 module.exports = calculator;
