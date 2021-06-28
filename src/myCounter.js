caa9f7a0e0a3facc61af695d1d3146fe3b6e11f5
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-06-28 15:13:05 -0300

    Corrige o requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,8 +20,8 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let count = 2; count <= 3; count += 1) {
+      myArray.push(count);
     }
   }
   return myArray;
