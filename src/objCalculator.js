218c0d54387de955d3dfb6eb66a0ab9028c188d1
Gabriel Priss <gabrielpriss@hotmail.com>
2021-06-27 21:52:12 -0300

    Resolve erros de lint

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,10 +20,10 @@
 */
 
 const calculator = {
-  add: (number1,number2) => (number1 + number2),
-  mult: (number1,number2) => (number1 * number2),
-  div: (number1,number2) => Math.floor(number1 / number2),
-  sub: (number1,number2) => (number1 - number2),
+  add: (number1, number2) => (number1 + number2),
+  mult: (number1, number2) => (number1 * number2),
+  div: (number1, number2) => Math.floor(number1 / number2),
+  sub: (number1, number2) => (number1 - number2),
 };
 
 module.exports = calculator;
