eed29eb626d1fe092f6732799699e84eff3fc988
Max Salewski <maxsalewski@gmail.com>
2021-06-28 16:25:02 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const studant = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return studant;
+};
 
 module.exports = createStudent;
