7ccc73c06918d2a48fc2da7f5717a974f02baba8
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-06-27 22:56:54 -0300

    consertos do lint requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = nome => estudante = { name: nome, feedback: () => "Eita pessoa boa!"};
+const createStudent = (nome) => { 
+ const estudante = { name: nome, feedback: () => 'Eita pessoa boa!' }; 
+ return estudante;
+};
 
 module.exports = createStudent;
