d04bab618bd7d1448f87f2ec6c05a987508641d6
Adriana Barreto <adri92barreto123@gmail.com>
2021-06-27 22:23:39 -0300

    Executa sexta questao com ajuda de Josue Lobo

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (student) => {
+  const dadosEstudantes = {
+    name: student,
+    feedback: () => 'Eita pessoa boa!'
+  }
+  return dadosEstudantes;
+};
 
 module.exports = createStudent;
