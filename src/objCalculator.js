4b6634d24ed52bda601eb4920f30405555f92489
Eduardomak <eduardomak92@gmail.com>
2021-06-29 04:00:11 -0300

    corrigindo 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -25,7 +25,7 @@ const addc = (num1, num2) => (num1 + num2);
 const multp = (num1, num2) => (num1 * num2);
 const subt = (num1, num2) => (num1 - num2);
 const divis = (num1, num2) => {
-  Math.floor(num1 / num2);
+  return Math.floor(num1 / num2);
 };
 calculator.add = addc;
 calculator.mult = multp;
