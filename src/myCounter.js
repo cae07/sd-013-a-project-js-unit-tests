54fedc3848a844e64ca2b0bfee8400f9403e409c
Ygor <ygormaiaa1@gmail.com>
2021-06-28 23:57:25 -0300

    Alterando lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,7 +18,7 @@
 const myCounter = () => {
   var myArray = [];
   for (let index = 0; index <= 3; index += 1) {
-    myArray.push(counter);
+    myArray.push(index);
     for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
