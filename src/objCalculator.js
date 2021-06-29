18bc8d5906db15013e83e7a59b12bd60151ab56d
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-06-29 20:18:29 -0300

    resolve requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,12 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+// const calculator = {};
+const calculator = {
+  add: (num1, num2) => num1 + num2,
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => Math.round((num1 / num2) - 0.1),
+  sub: (num1, num2) => num1 - num2,
+};
 
 module.exports = calculator;
