23452ac7e0a29e84a99a1dda2e7807d3f50c2820
junglejf <jfcchagas@id.uff.br>
2021-06-28 21:12:05 -0300

    ex6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,19 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const pd = productDetails('uva','pera');
+    assert.strictEqual(Array.isArray(pd), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(pd.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(pd[0] instanceof Object && pd[1] instanceof Object, true);
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(pd[0] === pd[1], false);
     // Teste que os dois productIds terminam com 123.
+    const pd1 = pd[0].details.productId.endsWith("123");
+    const pd2 = pd[1].details.productId.endsWith("123");
+    assert.strictEqual(pd1 && pd2 && true, true);
   });
 });
