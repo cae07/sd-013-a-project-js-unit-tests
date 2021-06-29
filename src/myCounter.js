2f636d0f73b221e0e2f1a8d8a05d7261de8520fd
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-06-29 11:52:05 -0300

    Requisito 8 :rocket:

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var count = 2; count <= 3; count += 1) {
+      myArray.push(count);
     }
   }
   return myArray;
