2acb8ae72ca16b0c7c6552aab8f77d4305d15214
junglejf <jfcchagas@id.uff.br>
2021-06-28 21:31:42 -0300

    falta ex9

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,13 +14,10 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
+const fb = () => 'Eita pessoa boa!';
 
-const createStudent = (name) => {
-  
-  return {
-    name: name,
-    feedback: feedback = () => 'Eita pessoa boa!',
-
-};
-
+const createStudent = (name) => ({
+    name,
+    feedback: fb,
+  });
 module.exports = createStudent;
--- src/myCounter.js
+++ src/myCounter.js
@@ -16,10 +16,10 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let i = 0; i <= 3; i += 1) {
+    myArray.push(i);
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
