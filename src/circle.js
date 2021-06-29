ea38a9e24e2349e86bdc96895563a541e75e88f3
thomasravache <thomasravache31@gmail.com>
2021-06-29 13:41:10 -0300

    Alterando requisito 04

--- src/circle.js
+++ src/circle.js
@@ -23,7 +23,7 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: parseFloat((PI * radius * radius).toFixed(2)),
+    area: parseFloat((PI * radius * radius)),
     circumference: 2 * PI * radius,
   };
 };
