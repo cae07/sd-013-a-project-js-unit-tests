9f65b05f26989d8de48dc65d00f6a41bcc98b40d
Breno Lopes <brenokos4@gmail.com>
2021-06-29 01:14:31 -0300

    teste

--- src/average.js
+++ src/average.js
@@ -17,13 +17,11 @@ const average = (arr = Array) => {
   let soma = null;
   if (arr.length !== 0) {
     for (let i = 0; i < arr.length; i += 1) {
-      if (typeof arr[i] === 'string') {
+      if (typeof arr[i] !== 'number') {
         return undefined;
       }
       soma += arr[i] / arr.length;
     }
-  } else {
-    return undefined;
   }
   return Math.round(soma);
 };
