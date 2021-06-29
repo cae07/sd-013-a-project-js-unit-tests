c381e8d9ebe47a1d93835c8e000fb539688b3d33
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-06-29 01:04:11 -0300

    Adiciona requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,11 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var addTwoNumbers = 2; addTwoNumbers <= 3; addTwoNumbers += 1) {
+      myArray.push(addTwoNumbers);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
