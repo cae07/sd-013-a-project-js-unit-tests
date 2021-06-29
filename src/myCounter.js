4ff29ce82d58fe23ff15f4e882b863c03d4bcdbe
Caê <eduardokae01@hotmail.com>
2021-06-29 13:50:13 -0300

    arruma lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,13 +15,13 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-//  Consulta em: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+//  Consulta em: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sliceadd .
 const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   for (let counter = 1; counter <= 3; counter += 1) {
