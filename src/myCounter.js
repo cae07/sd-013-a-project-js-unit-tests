dee9a783bdf70db28ca06e5db1da87e912def00e
Caê <eduardokae01@hotmail.com>
2021-06-29 13:46:58 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,14 +15,19 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
+//  Consulta em: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
+  for (let counter = 1; counter <= 3; counter += 1) {
+    myArray.push(counter);
+  }
+  myArray = myArray.slice(0, 12);
   return myArray;
 };
 
