deb9169375a9c6c724db07104061a2a5e8514407
Lucas Oliveira Sousa Dias Rosendo <lucasrosendo91@gmail.com>
2021-06-29 05:26:16 -0300

    PD

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,19 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    
     // ESCREVA SEUS TESTES ABAIXO:
+    const product = productDetails('produto1', 'produto2');
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(product), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(product.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof product, 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(product[0], product[1]);
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(product[0].details.productId.substr(-3), '123');
+    assert.strictEqual(product[1].details.productId.substr(-3), '123');
   });
 });
