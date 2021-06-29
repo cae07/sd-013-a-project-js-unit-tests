fa2daae1afcd1e619728904ef116de42ac965c14
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-06-29 02:01:48 -0300

    concluido requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,25 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    //assert.fail();
+    const product = productDetails('Alcool gel', 'Máscara');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof(product),'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof(productDetails('Alcool gel', 'Máscara')),'object');
     // Teste que os dois objetos são diferentes entre si.
+    const different = (product) => {
+      if (product[1] !== product[0]) {
+        return true;
+      }
+      return false;
+    };
+    assert.strictEqual(different(product), true);
     // Teste que os dois productIds terminam com 123.
+    // referemcoa dp substr https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substr
+    assert.strictEqual(product[0].details.productId.substr(-3) && product[1].details.productId.substr(-3), '123');
   });
 });
