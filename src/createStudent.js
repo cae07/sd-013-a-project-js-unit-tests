831b8b5a4b735bd9c04f2abb07f1afb704c5d899
Edgar Melo <edgarmeloam@gmail.com>
2021-07-02 20:24:42 -0300

    Implementa requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const dados = {
+    name,
+    feedback() { return 'Eita pessoa boa!'; },
+  };
+    return dados;
+  };
 
 module.exports = createStudent;
