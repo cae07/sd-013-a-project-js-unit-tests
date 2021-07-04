be242120a7cf748bdec2ccb7067c2da087454f73
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-07-04 16:56:02 -0300

    Corrige o lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,7 +19,7 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let index  = 2; index <= 3; index += 1) {
+    for (let index = 2; index <= 3; index += 1) {
       myArray.push(index);
     }
   }
