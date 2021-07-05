7e51bb1c1574451af548b7e98ab3739260f602d5
thomasravache <thomasravache31@gmail.com>
2021-07-05 16:12:55 -0300

    Adicionando teste 05 do requisito 09

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -84,7 +84,9 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // TESTE 5: Verifique que chamar uma função associada à chave `order` no objeto retornado, passando uma string como parâmetro, 
     // como `objetoRetornado.order('coxinha')`, tal string é adicionada ao array retornado em `objetoRetornado.consumption
     // ```
-    // const objetoRetornado = createMenu(objetoQualquer);
+    const objetoRetornado5 = createMenu({food: 'coxinha', drink: 'coca'});
+    objetoRetornado5.order('coxinha');
+    assert.deepStrictEqual(objetoRetornado5.comsuption, ['coxinha']);
     // objetoRetornado.order("coxinha");
     // objetoRetornado.consumption // Retorno: ["coxinha"]
     // ```
