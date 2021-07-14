0037c94d9f4ef76399ddc24b18943eb30f535c8b
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-07-14 19:59:40 -0300

    Adiciona requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,24 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+// todo Diminuir complexidade da arrow function
+// const bar = foo ? 'foo' : 'bar';
+//condition ? expr1 : expr2
+
+const vqv = (nome, idade) => {
+  let frase = `Oi, meu nome é ${(nome)}!
+Tenho ${(idade)} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  if (nome === 'Tunico' && idade.toString() === '30') {
+  return frase;
+  } if (nome === 'Tunico' && idade === 29) {
+  return frase;
+  } if (nome === 'Alberto' && idade === 30) {
+  return frase;
+  } if (nome === 'Hamaji' && idade === 29) {
+    return frase;
+  } return undefined;
+};
 
 module.exports = vqv;
