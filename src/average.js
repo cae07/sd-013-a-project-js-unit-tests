6889caae121c60588836c888761104ccd2c0de32
Antonio Martins <anmmartyns@gmail.com>
2021-07-15 15:20:16 -0300

    Requisito 1 refatorado

--- src/average.js
+++ src/average.js
@@ -16,17 +16,16 @@ const average = (array) => {
   if (array.length === 0) {
     return undefined;
   }
-  let soma = 0;
-  let media = 0;
+
   for (let index = 0; index < array.length; index += 1) {
     if (typeof (array[index]) !== 'number') {
       return undefined;
-    } else {
-      soma += array[index];
     }
   }
-  media = soma / array.length;
+
+  const soma = array.reduce(function (a, b) { return a + b; });
+  const media = soma / (array.length);
   return Math.round(media);
 };
-console.log()
+
 module.exports = average;
