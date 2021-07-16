d6cac56bfe52db132f3988f86b858cb9c7ff474c
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-07-16 19:18:52 -0300

    Adiciona requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var secondCounter = 2; secondCounter <= 3; secondCounter += 1) {
+      myArray.push(secondCounter);
     }
   }
   return myArray;
