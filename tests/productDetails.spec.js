942623464b4b44f233d69bc10538365c892b9a24
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-07-16 17:50:11 -0300

    Adiciona requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,17 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.ok(Array.isArray(productDetails()));
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.keys(productDetails()).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof Object.keys(productDetails()), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notEqual(Object.is(productDetails()), true);
     // Teste que os dois productIds terminam com 123.
+    assert.ok(Object.values(productDetails())[1].details.productId.includes('123'));
   });
 });
