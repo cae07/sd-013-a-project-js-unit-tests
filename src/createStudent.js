0f4d66585f21995b3292ed89859ade10b0e106fe
João Victor Paduam <joao.paduam@hotmail.com>
2021-07-20 19:09:10 -0300

    Conclusao da createStudents e myCounter

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,10 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
-  return{ 
-    name: name,
-    feedback: 'Eita pessoa boa!'
+const createStudent = (nome) => {
+  const objeto = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+    };
+  return objeto;
   };
-}
+
 module.exports = createStudent;
