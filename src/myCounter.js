e222a2c8164e14fdc186f7cbbab6fa57494d3e4f
Xenon-Neko <darkankher@gmail.com>
2021-07-21 03:09:30 -0300

    Counter Function V3

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   const myArray = [];
   for (let counter1 = 0; counter1 <= 3; counter1 += 1) {
     myArray.push(counter1);
-    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter2);
+  for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+    myArray.push(counter2);
     }
   }
   return myArray;
