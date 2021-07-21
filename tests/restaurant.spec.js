cede27fd07b87c9c8ff05db870af6bda6889d256
Xenon-Neko <darkankher@gmail.com>
2021-07-21 03:22:19 -0300

    Restaurant Function and Test V1

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -52,6 +52,8 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
     // ```
+    const menu = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };
+    assert.deepStrictEqual(typeof createMenu(menu), 'object');
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
