595cb3856bbb86047c729de42c1b5a72ebb6acaa
Cae <eduardokae01@hotmail.com>
2021-10-18 00:41:36 -0300

    implementa funcao average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) return undefined;
+  let sumArray = 0;
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sumArray += array[index];
+  }
+  return Math.round(sumArray / array.length);
+};
+
+console.log(average([3, 4, 5]));
 
 module.exports = average;
